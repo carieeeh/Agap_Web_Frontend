359165780e22d25700976a0b7a1db8f1f750f586
Reydan <reydanjohnbelen@gmail.com>
2024-02-12 23:04:04 +0800

    login setup change

---
+++ src/views/Layout/Public/LoginLogo.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex justify-center col-span-2 relative">
+        <img class="h-20 absolute top-52 left-96" src="/assets/images/gif/flood.gif" alt="" />
+        <img class="h-20 absolute bottom-72 left-36" src="/assets/images/gif/humanitarian-help.gif" alt="" />
+        <img class="h-20 absolute top-48 left-1/3" src="/assets/images/gif/fire_building.gif" alt="" />
+        <img class="h-20 absolute bottom-40 left-10" src="/assets/images/gif/first-aid-kit.gif" alt="" />
+        <img class="h-20 absolute bottom-48 left-64" src="/assets/images/gif/security-guard.gif" alt="" />
+        <img class="h-20 absolute bottom-56 left-96" src="/assets/images/gif/charity.gif" alt="" />
+        <img class="h-20 absolute bottom-32 right-10" src="/assets/images/gif/ambulance.gif" alt="" />
+        <img class="h-20 absolute top-56 left-10" src="/assets/images/gif/earthquake_colored.gif" alt="" />
+        <img class="h-44" src="/assets/images/red_no_bg_logo.png" alt="" />
+    </div>
+</template>
