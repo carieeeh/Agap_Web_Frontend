3c0169bd40014bbe5ffaa01069176f7be16a6caa
Reydan <reydanjohnbelen@gmail.com>
2024-02-12 23:03:25 +0800

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection } from 'firebase/firestore'
 // ... other firebase imports
 
-const firebaseConfig = {
+export const firebaseApp = initializeApp({
   apiKey: "AIzaSyBgXlnez0RJ1GffBAeWjPDKNqI2Ad1DXAA",
   authDomain: "agap-f4c32.firebaseapp.com",
+  databaseURL: "https://agap-f4c32-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "agap-f4c32",
   storageBucket: "agap-f4c32.appspot.com",
   messagingSenderId: "1060919755818",
   appId: "1:1060919755818:web:236f2e1b76c386d7516f53",
-  measurementId: "G-J7L99MFPGV",
-};
-
-export const firebaseApp = initializeApp({ firebaseConfig });
+  measurementId: "G-J7L99MFPGV"
+})
 
 // used for the firestore refs
-const db = getFirestore(firebaseApp);
+const db = getFirestore(firebaseApp)
 
 // here we can export reusable database references
-export const todosRef = collection(db, "todos");
+export const todosRef = collection(db, 'todos')
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { firebaseApp } from './firebase'
+import { firebaseApp } from '@/firebase'
 
 
 const app = createApp(App)
