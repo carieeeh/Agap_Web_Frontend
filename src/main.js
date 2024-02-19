91abee7ec8fa3e4744d72e3046fca9a2d8e3b647
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-02-19 21:42:03 +0800

    pinia authetication

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "./assets/main.css";
 import { VueFire, VueFireAuth } from "vuefire";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPersist from 'pinia-plugin-persist'
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,9 +11,12 @@ import { firebaseApp } from "@/firebase";
 import Vueform from "@vueform/vueform";
 import vueformConfig from "./../vueform.config";
 
+
+const pinia = createPinia();
+pinia.use(piniaPersist);
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(VueFire, {
   // imported above but could also just be created here
