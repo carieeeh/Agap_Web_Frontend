ec9e7e2181112772169b2ef371479abb2b825ee1
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-03-28 17:49:41 +0800

    fix report list

--- src/views/Layout/Private/Dashboard/LineChart.vue
+++ src/views/Layout/Private/Dashboard/LineChart.vue
@@ -49,19 +49,11 @@ const chartData = reactive({
 
 const chartOptions = { responsive: true };
 
-const stats = [
-  { name: "Fire Reports", stat: "5", color: "border-b-primaryRed", image: "/assets/images/gif/fire_building.gif" },
-  { name: "Earthquake Reports", stat: "0", color: "border-b-primaryYellow", image: "/assets/images/gif/earthquake_colored.gif" },
-  { name: "Medical Reports", stat: "3", color: "border-b-green-500", image: "/assets/images/gif/first-aid-kit.gif" },
-  { name: "Flood Reports", stat: "1", color: "border-b-blue-600", image: "/assets/images/gif/flood.gif" },
-  { name: "Police Reports", stat: "10", color: "border-b-sky-400", image: "/assets/images/gif/security-guard.gif" },
-];
-
 </script>
 
 <template>
     <div>
-        <ReportList :stats="stats" />
+        <ReportList />
         <div class="flex justify-center items-center w-full px-5 mt-5">
             <Line :data="chartData" :options="chartOptions" aria-label="Accident Reports Chart" />
         </div>
--- src/views/Layout/Private/InteractiveMap/InteractiveMap.vue
+++ src/views/Layout/Private/InteractiveMap/InteractiveMap.vue
@@ -1,18 +1,11 @@
 <script setup>
 import ReportList from "@/views/components/List/ReportList.vue";
 
-const stats = [
-  { name: "Fire Reports", stat: "5", color: "border-b-primaryRed", image: "/assets/images/gif/fire_building.gif" },
-  { name: "Earthquake Reports", stat: "0", color: "border-b-primaryYellow", image: "/assets/images/gif/earthquake_colored.gif" },
-  { name: "Medical Reports", stat: "3", color: "border-b-green-500", image: "/assets/images/gif/first-aid-kit.gif" },
-  { name: "Flood Reports", stat: "1", color: "border-b-blue-600", image: "/assets/images/gif/flood.gif" },
-  { name: "Police Reports", stat: "10", color: "border-b-sky-400", image: "/assets/images/gif/security-guard.gif" },
-];
 </script>
 
 <template>
   <div>
-    <ReportList :stats="stats" />
+    <ReportList />
 
     <div class="flex pt-5">
       <iframe
